49fbae457abce2bc22acc987989997ee78a25374
Esra404 <esradrmz571@gmail.com>
2024-03-20 00:53:08 +0300

    php temelleri

--- değişkenler.php
+++ değişkenler.php
@@ -10,6 +10,22 @@ $sayi2=150;
 echo $sayi1+$sayi2;
 echo "<br>";
 echo $sayi1." ".$sayi2;
-
-  
+// bu bir yorum satırı php de ctrl+k+c ile kısaca oluşturabilirim k+u ile geri alınabilir
+// değişkenn isminde boşluk karakteri kulanılamaz
+// değikwnler sayı ile başlayamaz
+// değişkwnlwe büyük küçük harf duyarlıdır
+//PHP VERİ TİPLERİ
+/**
+ * String
+ * $urunAdi="samsung";
+ * int
+ * $fiyat=123;
+ * double
+ * $kdvOrani=12.12;
+ * boolean
+ * $satistaMi=true;
+ * object
+ * array
+ * null
+ */
 ?>
